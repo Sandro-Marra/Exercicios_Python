ebb135625a214c7e290ec63ae46406f6ad068042
sandro marra <sandro.marra@luizalabs.com>
2022-09-18 15:17:17 -0300

    Exercício 5

--- 03_EstruturaDeRepeticao/05.py
+++ 03_EstruturaDeRepeticao/05.py
@@ -1,22 +1,31 @@
 # Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
 
-pais_a = int(input('Informe a população do pais A: '))
-tx_a = float(input('Informa a tava de crescimento do pais A: '))
-pais_b = int(input('Informe a população do pais B:'))
-tx_b = float(input('Informa a tava de crescimento do pais A: '))
+while True:
+    pais_a = int(input('Informe a população do pais A: '))
+    tx_a = float(input('Informa a tava de crescimento do pais A: '))
+    pais_b = int(input('Informe a população do pais B: '))
+    tx_b = float(input('Informa a tava de crescimento do pais A: '))
 
-ano = 0 #simular a passagem de anos
-while not (pais_a >= pais_b):
-#while pais_a < pais_b:
-    pais_a = round(pais_a * tx_a + pais_a)
-    pais_b = round(pais_b * tx_b + pais_b)
-    ano = ano + 1
+    ano = 0 #simular a passagem de anos
+    while not (pais_a >= pais_b):
+    #while pais_a < pais_b:
+     pais_a = round(pais_a * tx_a + pais_a)
+     pais_b = round(pais_b * tx_b + pais_b)
+     ano = ano + 1
 
-print(f'''
+    print(f'''
 A população do País A de {pais_a} habitantes
 ultrapassou o País B de {pais_b} habitantes
 após {ano} anos.
 ''')
-# repetir = input('Deseja repetir a operação, S ou N? ').lower()
-# if repetir == "s": 
 
+    repetir = input('Deseja repetir a operação, S ou N? ').lower()
+    while not repetir in 's,n': 
+        print('Opção inválida, informe s ou n.')
+        repetir = input('Deseja repetir a operação: ').lower()
+    if repetir == 's':
+        continue
+    else:
+        repetir == 'n'
+        print('''\nObrigado!''')
+        break
